73ece772861a596869d5d7e92a6709128103893a
bikebilly <fabio@gitlab.com>
2017-06-28 14:28:46 +0200

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,13 +1,11 @@
 package com.example.app;
 
-/**
- * Hello world!
- *
- */
+import com.example.dep.Dep;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Dep.hello( "GitLab" );
     }
 }
